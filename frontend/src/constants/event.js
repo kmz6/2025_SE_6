efa6013d5b22dd1f19621b2045d25534bb0cc095
gpdnjs8 <jade3735@naver.com>
2025-06-09 15:55:49 +0900

    feat: 시험기간 라벨 표시

--- frontend/src/constants/event.js
+++ frontend/src/constants/event.js
@@ -1,4 +1,7 @@
-export const examPeriods = [{ start: "2025-06-10", end: "2025-06-16" }];
+export const examPeriods = [
+  { start: "2025-04-22", end: "2025-04-28", label: "중간고사" },
+  { start: "2025-06-10", end: "2025-06-16", label: "기말고사" },
+];
 
 export const festivalPeriod = {
   start: "2025-05-21",
